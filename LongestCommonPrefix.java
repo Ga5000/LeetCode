145cdcdef7d0108a19b83c62e771729d61d5853e
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-08-22 19:16:42 -0300

    new solutions

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
     public static String longestCommonPrefix(String[] strs) {
-        
-        return "";
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length -1];
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < Math.min(first.length(),last.length()); i++){
+            if(first.charAt(i) != last.charAt(i)){
+                return result.toString();
+            }
+                result.append(first.charAt(i));
+        }
+        return result.toString();
     }
     public static void main(String[] args) {
         String[] strs = {"flower","flow","flight"};
