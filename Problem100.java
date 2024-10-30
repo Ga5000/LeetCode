0317491f2433b7a5901b6a04db1d8b6e90cfed32
Gabriel Lisboa <gbr.lisboa@gmail.com>
2024-10-30 17:39:23 -0300

    new problem solved

---
+++ Problem100.java
@@ -0,0 +1,14 @@
+public class Problem100 {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        TreeNode q = new TreeNode(2);
+
+
+    }
+}
